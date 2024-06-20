764cb308911dcee17766941d5c9d1d76d8329596
Y1-Bit <y0749701@gmail.com>
2024-06-20 18:42:58 +0300

    Predator and Herbivore move logic

--- entities/herbivore.py
+++ entities/herbivore.py
@@ -1,3 +1,6 @@
+from map import Map
+from entities import Grass
+
 from .creatures import Creature
 
 
@@ -5,5 +8,18 @@ class Herbivore(Creature):
     def __init__(self, position: tuple[int, int], speed: int, hp: int) -> None:
         super().__init__(position, speed, hp)
 
-    def make_move(self) -> None:
+    def make_move(self, game_map: Map) -> None:
+        grass_position = self.find_closest(game_map, Grass)
+        print(grass_position)
+        if grass_position:
+            self.move_towards(game_map, grass_position)
+            # TODO: Check Description
+            if self.position == grass_position:
+                grass = game_map.get_entity(grass_position)
+                if isinstance(grass, Grass):
+                    self.eat(grass, game_map)
         print('Herbivore moves')
+        
+    def eat(self, grass: Grass, game_map: Map) -> None:
+        print('Herbivore eats grass.')
+        game_map.remove_entity(grass)
--- entities/predator.py
+++ entities/predator.py
@@ -1,3 +1,5 @@
+from map import Map
+
 from .creatures import Creature
 from .herbivore import Herbivore
 
@@ -7,10 +9,18 @@ class Predator(Creature):
         super().__init__(position, speed, hp)
         self.attack_power: int = attack_power
 
-    def make_move(self) -> None:
-        print('Predator moved to hunt.')
-    
-    def attack(self, herbivore: Herbivore) -> None:
-        herbivore.hp -= self.attack_power 
+    def make_move(self, game_map: Map) -> None:
+        herbivore_position = self.find_closest(game_map, Herbivore)
+        if herbivore_position:
+            self.move_towards(game_map, herbivore_position)
+            if self.position == herbivore_position:
+                herbivore = game_map.get_entity(herbivore_position)
+                if isinstance(herbivore, Herbivore):
+                    self.attack(herbivore, game_map)
+        print('Predator moves to hunt.')
+
+    def attack(self, herbivore: Herbivore, game_map: Map) -> None:
+        herbivore.hp -= self.attack_power
         if herbivore.hp <= 0:
             print('Herbivore has been killed.')
+            game_map.remove_entity(herbivore)
