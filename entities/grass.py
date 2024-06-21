51434ccbb7310959d8e9eff4a31f98f6f6532ec6
Y1-Bit <y0749701@gmail.com>
2024-06-21 21:41:10 +0300

    Moved Grass to Static Objects

--- entities/grass.py
+++ entities/grass.py
@@ -1,6 +1,3 @@
 from .entity import Entity
 
 
-class Grass(Entity):
-    def __init__(self, position: tuple[int, int]) -> None:
-        super().__init__(position)
--- entities/static_objects.py
+++ entities/static_objects.py
@@ -14,3 +14,8 @@ class Rock(StaticObject):
 class Tree(StaticObject):
     def __init__(self, position: tuple[int, int]) -> None:
         super().__init__(position)
+
+
+class Grass(Entity):
+    def __init__(self, position: tuple[int, int]) -> None:
+        super().__init__(position)
