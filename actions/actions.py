1bcd72bb3ede3aef35e8ed2030b619374df3f96e
Y1-Bit <y0749701@gmail.com>
2024-06-29 22:59:27 +0300

    Update Init Action

--- actions/actions.py
+++ actions/actions.py
@@ -15,21 +15,29 @@ class Action(ABC):
 
 class InitAction(Action):
     def execute(self, game_map: Map) -> None:
-        for _ in range(4):  
-            x, y = random.randint(0, game_map.width - 1), random.randint(0, game_map.height - 1)
-            game_map.add_entity(Grass((x, y)))
-        for _ in range(5): 
-            x, y = random.randint(0, game_map.width - 1), random.randint(0, game_map.height - 1)
-            game_map.add_entity(Rock((x, y)))
-        for _ in range(5):  
-            x, y = random.randint(0, game_map.width - 1), random.randint(0, game_map.height - 1)
-            game_map.add_entity(Tree((x, y)))
-        
-        # TODO: Creature can't spawn in filled cell
-        initial_herbivore_position = (random.randint(0, game_map.width - 1), random.randint(0, game_map.height - 1))
-        game_map.add_entity(Herbivore(initial_herbivore_position, speed=1, hp=30))
-        initial_predator_position = (random.randint(0, game_map.width - 1), random.randint(0, game_map.height - 1))
-        game_map.add_entity(Predator(initial_predator_position, speed=2, hp=15, attack_power=5))
+        self.spawn_static_entities(game_map, Grass, 4)
+        self.spawn_static_entities(game_map, Rock, 5)
+        self.spawn_static_entities(game_map, Tree, 5)
+        self.spawn_creature(game_map, Herbivore, 1, 30)
+        self.spawn_creature(game_map, Predator, 2, 15, 5)
+
+    def spawn_static_entities(self, game_map: Map, entity_type: type, count: int) -> None:
+        for _ in range(count):
+            while True:
+                x, y = random.randint(0, game_map.width - 1), random.randint(0, game_map.height - 1)
+                if not game_map.get_entity((x, y)):
+                    game_map.add_entity(entity_type((x, y)))
+                    break
+
+    def spawn_creature(self, game_map: Map, creature_type: type, speed: int, hp: int, attack_power: int | None = None) -> None:
+        while True:
+            position = (random.randint(0, game_map.width - 1), random.randint(0, game_map.height - 1))
+            if not game_map.get_entity(position):
+                if attack_power:
+                    game_map.add_entity(creature_type(position, speed=speed, hp=hp, attack_power=attack_power))
+                else:
+                    game_map.add_entity(creature_type(position, speed=speed, hp=hp))
+                break
 
 
 class TurnAction(Action):
