fa7134e6497cb8c57722451d439493f9c9f62290
Y1-Bit <y0749701@gmail.com>
2024-06-30 17:46:59 +0300

    Simulation Update

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -14,27 +14,34 @@ class Simulation:
         self.turn_actions: list[Action] = [TurnAction()]
         self.health_stats: dict[str, int] = {}
         self.creatures = []
+        self.is_paused = False
     
     def next_turn(self) -> None:
-        for action in self.turn_actions:
-            action.execute(self.map)
-        self.move_counter += 1
-        self.update_health_stats() 
-        self.renderer.render(self.map, self.health_stats)
+        if not self.is_paused:  
+            for action in self.turn_actions:
+                action.execute(self.map)
+            self.move_counter += 1
+            self.update_health_stats() 
+            self.renderer.render(self.map, self.health_stats, self.move_counter)
 
     def start_simulation(self) -> None:
         for action in self.init_actions:
             action.execute(self.map)
-        self.creatures = self.map.get_all_creatures()
-        self.update_health_stats()
-        self.renderer.render(self.map, self.health_stats)
-        while True:
-            time.sleep(1)
-            self.next_turn()
 
+        entities = self.map.get_entities()
+        for entity in entities.values():
+            if hasattr(entity, 'hp'):
+                self.creatures.append(entity)
+        
+        while True:
+            self.next_turn()
+            time.sleep(1)
         
     def update_health_stats(self) -> None:
         self.health_stats = {creature.__class__.__name__: creature.hp for creature in self.creatures}
 
     def pause_simulation(self) -> None:
-        pass
+        self.is_paused = True  
+
+    def resume_simulation(self) -> None:
+        self.is_paused = False  
