2d0a441cde55b52425094e3588e8cbf417dea6a6
Y1-Bit <y0749701@gmail.com>
2024-07-03 12:32:16 +0300

    EntityAction class

--- actions/actions.py
+++ actions/actions.py
@@ -10,18 +10,8 @@ class Action(ABC):
     def execute(self, game_map: Map) -> None:
         pass
 
-    def spawn_static_entities(
-        self, game_map: Map, entity_type: type, count: int
-    ) -> None:
-        for _ in range(count):
-            while True:
-                x, y = random.randint(0, game_map.width - 1), random.randint(
-                    0, game_map.height - 1
-                )
-                if not game_map.get_entity((x, y)):
-                    game_map.add_entity(entity_type((x, y)))
-                    break
 
+class EntityActions(Action):
     def spawn_creature(
         self,
         game_map: Map,
@@ -46,15 +36,27 @@ class Action(ABC):
                     game_map.add_entity(creature_type(position, speed=speed, hp=hp))
                 break
 
+    def spawn_static_entities(
+        self, game_map: Map, entity_type: type, count: int
+    ) -> None:
+        for _ in range(count):
+            while True:
+                x, y = random.randint(0, game_map.width - 1), random.randint(
+                    0, game_map.height - 1
+                )
+                if not game_map.get_entity((x, y)):
+                    game_map.add_entity(entity_type((x, y)))
+                    break
 
-class InitAction(Action):
+
+class InitAction(EntityActions):
     def execute(self, game_map: Map) -> None:
-        self.spawn_static_entities(game_map, Grass, 4)
-        self.spawn_static_entities(game_map, Rock, 5)
-        self.spawn_static_entities(game_map, Tree, 5)
         self.spawn_creature(game_map, Herbivore, 1, 30)
         self.spawn_creature(game_map, Herbivore, 1, 30)
         self.spawn_creature(game_map, Predator, 2, 15, 5)
+        self.spawn_static_entities(game_map, Grass, 4)
+        self.spawn_static_entities(game_map, Rock, 5)
+        self.spawn_static_entities(game_map, Tree, 5)
 
 
 class TurnAction(Action):
@@ -65,7 +67,7 @@ class TurnAction(Action):
                 entity.make_move(game_map)
 
 
-class GenerateGrassAction(Action):
+class GenerateGrassAction(EntityActions):
     def execute(self, game_map: Map) -> None:
         grass_count = len(game_map.grasses)
         print(grass_count)
@@ -73,7 +75,7 @@ class GenerateGrassAction(Action):
             self.spawn_static_entities(game_map, Grass, 4)
 
 
-class CheckAndSpawnAction(Action):
+class CheckAndSpawnAction(EntityActions):
     def execute(self, game_map: Map) -> None:
         creatures = [
             entity
