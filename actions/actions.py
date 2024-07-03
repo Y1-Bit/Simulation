12a1b89439af0e2eb017623560170e0e4c6d39df
Y1-Bit <y0749701@gmail.com>
2024-07-03 13:43:15 +0300

    chore: Update entity spawning logic in InitAction class

--- actions/actions.py
+++ actions/actions.py
@@ -15,18 +15,20 @@ class EntityActions(Action, ABC):
     def spawn_creature(
         self,
         game_map: Map,
-        creature_type: type,
-        speed: int,
-        hp: int,
-        attack_power: int | None = None,
+        creature_params: dict,
     ) -> None:
+        creature_type: type = creature_params['type']
+        speed: int = creature_params['speed']
+        hp: int = creature_params['hp']
+        attack_power: int | None = creature_params.get('attack_power')
+
         while True:
             position = (
                 random.randint(0, game_map.width - 1),
                 random.randint(0, game_map.height - 1),
             )
             if not game_map.get_entity(position):
-                if attack_power:
+                if attack_power is not None:
                     game_map.add_entity(
                         creature_type(
                             position, speed=speed, hp=hp, attack_power=attack_power
@@ -50,13 +52,28 @@ class EntityActions(Action, ABC):
 
 
 class InitAction(EntityActions):
+    def __init__(
+        self,
+        herbivore_params: dict,
+        predator_params: dict,
+        grass_count: int,
+        rock_count: int,
+        tree_count: int
+    ) -> None:
+        self.herbivore_params = herbivore_params
+        self.predator_params = predator_params
+        self.grass_count = grass_count
+        self.rock_count = rock_count
+        self.tree_count = tree_count
+
     def execute(self, game_map: Map) -> None:
-        self.spawn_creature(game_map, Herbivore, 1, 30)
-        self.spawn_creature(game_map, Herbivore, 1, 30)
-        self.spawn_creature(game_map, Predator, 2, 15, 5)
-        self.spawn_static_entities(game_map, Grass, 4)
-        self.spawn_static_entities(game_map, Rock, 5)
-        self.spawn_static_entities(game_map, Tree, 5)
+        for _ in range(self.herbivore_params['count']):
+            self.spawn_creature(game_map, self.herbivore_params)
+        for _ in range(self.predator_params['count']):
+            self.spawn_creature(game_map, self.predator_params)
+        self.spawn_static_entities(game_map, Grass, self.grass_count)
+        self.spawn_static_entities(game_map, Rock, self.rock_count)
+        self.spawn_static_entities(game_map, Tree, self.tree_count)
 
 
 class TurnAction(Action):
@@ -68,13 +85,19 @@ class TurnAction(Action):
 
 
 class GenerateGrassAction(EntityActions):
+    def __init__(self, generate_new_grass_count: int) -> None:
+        self.generate_new_grass_count = generate_new_grass_count
+    
     def execute(self, game_map: Map) -> None:
         grass_count = len(game_map.grasses)
         if grass_count == 0:
-            self.spawn_static_entities(game_map, Grass, 4)
+            self.spawn_static_entities(game_map, Grass, self.generate_new_grass_count)
 
 
 class CheckAndSpawnAction(EntityActions):
+    def __init__(self, check_and_spawn_params: dict) -> None:
+        self.check_and_spawn_params = check_and_spawn_params
+
     def execute(self, game_map: Map) -> None:
         creatures = [
             entity
@@ -82,5 +105,6 @@ class CheckAndSpawnAction(EntityActions):
             if isinstance(entity, (Herbivore, Predator))
         ]
         if len(creatures) <= 1:
-            self.spawn_creature(game_map, Herbivore, 1, 30)
-            self.spawn_creature(game_map, Herbivore, 1, 30)
+            for _ in range(self.check_and_spawn_params['count']):
+                self.spawn_creature(game_map, self.check_and_spawn_params)
+                
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from simulation import Simulation
 from actions import (
     Action,
@@ -7,19 +8,58 @@ from actions import (
     TurnAction,
 )
 from renderer import ConsoleRenderer
+from entities import Herbivore, Predator, Grass, Rock, Tree
 
+ENTITY_CLASSES = {
+    "Herbivore": Herbivore,
+    "Predator": Predator,
+    "Grass": Grass,
+    "Rock": Rock,
+    "Tree": Tree,
+}
+
+def load_config(config_path: str) -> dict:
+    with open(config_path, 'r') as file:
+        return json.load(file)
+
+def convert_params(params: dict) -> dict:
+    params['type'] = ENTITY_CLASSES[params['type']]
+    return params
 
 def main() -> None:
-    width, height = 10, 10
+    config = load_config('config.json')
+
+    width = config["simulation"]["width"]
+    height = config["simulation"]["height"]
     renderer = ConsoleRenderer()
-    init_actions: list[Action] = [InitAction()]
-    turn_actions: list[Action] = [TurnAction(), GenerateGrassAction(), CheckAndSpawnAction()]
+
+    herbivore_params = convert_params(config["init_entities"]["herbivore"])
+    predator_params = convert_params(config["init_entities"]["predator"])
+    static_counts = config["init_entities"]["static_counts"]
+
+    init_action = InitAction(
+        herbivore_params=herbivore_params,
+        predator_params=predator_params,
+        grass_count=static_counts["grass_count"],
+        rock_count=static_counts["rock_count"],
+        tree_count=static_counts["tree_count"],
+    )
+
+    generate_new_grass_count = config["actions"]["generate_new_grass_count"]
+
+    check_and_spawn_params = convert_params(config["actions"]["check_and_spawn_action"])
+
+    init_actions: list[Action] = [init_action]
+    turn_actions: list[Action] = [
+        TurnAction(),
+        CheckAndSpawnAction(check_and_spawn_params=check_and_spawn_params),
+        GenerateGrassAction(generate_new_grass_count),
+    ]
     simulation = Simulation(width, height, renderer, init_actions, turn_actions)
     try:
         simulation.start_simulation()
     except KeyboardInterrupt:
         renderer.game_stopped_message()
-
 
 if __name__ == "__main__":
     main()
